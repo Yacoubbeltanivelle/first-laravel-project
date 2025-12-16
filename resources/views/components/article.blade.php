40bdf5b2f0bf77fe97f3e198e800e882ccc71a83
Yacoubbeltanivelle <yacoubbeltanivelle@gmail.com>
2025-12-16 16:18:49 +0100

    ACTIVITE 11 : Créer un composant

---
+++ resources/views/components/article.blade.php
@@ -0,0 +1,4 @@
+<div style="border: 5px solid #055aecff; padding: 15px; margin-bottom: 10px;">
+    <h3 style="margin-top: 0; color: #a00303ff;">{{ $title }}</h3>
+    <p>{{ $description }}</p>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,17 @@
 @section('title', 'Accueil')
 @section('content')
 <h2>Bienvenue sur la page d’accueil</h2>
-<p>Ceci est le contenu principal de la page.</p>
+<div>
+    <x-article
+        title="L'IA soigne mieux"
+        description="L'intelligence artificielle aide les médecins à diagnostiquer plus vite." />
+
+    <x-article
+        title="Villes vertes"
+        description="Les métropoles deviennent plus écologiques et durables." />
+
+    <x-article
+        title="Télétravail"
+        description="Plus de liberté, mais aussi plus de solitude." />
+</div>
 @endsection
