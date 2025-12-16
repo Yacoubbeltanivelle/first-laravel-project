7d8b8552dc994fa7d446fe81b31835315124001c
Yacoubbeltanivelle <yacoubbeltanivelle@gmail.com>
2025-12-16 11:05:02 +0100

    Activité 7 : Nommage des routes home et article.details

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ArticleController;
 
-Route::get('/', [IndexController::class, 'index']);
-Route::get('/bonjour/{name}', [IndexController::class, 'bonjour']);
-Route::get('/articles/{id}', [ArticleController::class, 'show']);
+Route::get('/', [IndexController::class, 'index'])->name('home');
+Route::get('/bonjour/{name}', [IndexController::class, 'bonjour'])->name('bonjour');
+Route::get('/articles/{id}', [ArticleController::class, 'show'])->name('article.details');
