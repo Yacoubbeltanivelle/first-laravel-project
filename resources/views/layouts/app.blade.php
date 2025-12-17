0bc69271635c641ed7dfba0d319dbda807560658
Yacoubbeltanivelle <yacoubbeltanivelle@gmail.com>
2025-12-17 15:13:14 +0100

    ACTIVITE 16 : Importer le CSS et le JS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <title>@yield('title')</title>
     <link rel="icon" href="{{ asset('favicon.ico') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
