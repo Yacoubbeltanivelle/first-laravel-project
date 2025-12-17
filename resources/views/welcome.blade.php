782c5106f8755d0214c68957bf6a547e051b7f0e
Yacoubbeltanivelle <yacoubbeltanivelle@gmail.com>
2025-12-17 09:58:31 +0100

    ACTIVITE 12 : Rendre les articles via une boucle

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,16 +3,10 @@
 @section('content')
 <h2>Bienvenue sur la page d’accueil</h2>
 <div>
+    @foreach($articles as $article)
     <x-article
-        title="L'IA soigne mieux"
-        description="L'intelligence artificielle aide les médecins à diagnostiquer plus vite." />
-
-    <x-article
-        title="Villes vertes"
-        description="Les métropoles deviennent plus écologiques et durables." />
-
-    <x-article
-        title="Télétravail"
-        description="Plus de liberté, mais aussi plus de solitude." />
+        :title="$article['title']"
+        :description="$article['description']" />
+    @endforeach
 </div>
 @endsection
